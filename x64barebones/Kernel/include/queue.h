5337e36ade30b4a456bf2ee493f5b762ac68acba
Joaquin Battilana <battilanajn@gmail.com>
2019-05-18 19:15:45 -0300

    Created queueADT, not tested

---
+++ x64barebones/Kernel/include/queue.h
@@ -0,0 +1,19 @@
+#ifndef
+#define _QUEUE_H
+
+#include <mem_manager.h>
+
+typedef queueADT * queue_t;
+
+// creates a new queue
+queue_t new_queue();
+// add a element to queue
+queue_t enqueue(queue_t queue, void * elem);
+// removes element from queue and returns it
+void * dequeue(queue_t queue);
+// return first element of the queue
+void * peek(queue_t queue);
+// return size of the queue
+size_t get_queue_size(queue_t queue);
+
+#endif
