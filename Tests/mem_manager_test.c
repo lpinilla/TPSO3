d7d53c6738afc89c997fb72a62a7391ea6f4bbb3
lpinilla <lauti_nipi123@hotmail.com>
2019-05-19 12:53:50 -0300

    trabajando en el malloc

--- Tests/mem_manager_test.c
+++ Tests/mem_manager_test.c
@@ -15,9 +15,14 @@ void list_initial_populate_test3();
 void fix_size_test();
 void fix_size_test2();
 void fix_size_test3();
+void fix_size_test4();
 void power_of_2_check_test();
 void power_of_2_check_test2();
 void power_of_2_check_test3();
+void alloc_free_list_available_test();
+
+//helper functions
+void print_mem(size_t total_size);
 
 int main(void){
     create_suite("Testing the Memory Manager");
@@ -34,9 +39,11 @@ int main(void){
     add_test(fix_size_test);
     add_test(fix_size_test2);
     add_test(fix_size_test3);
+    add_test(fix_size_test4);
     add_test(power_of_2_check_test);
     add_test(power_of_2_check_test2);
     add_test(power_of_2_check_test3);
+    add_test(alloc_free_list_available_test);
     //correr la suite
     run_suite();
     //liberar los espacios
@@ -44,24 +51,24 @@ int main(void){
 }
 
 void powers_in_between_test(){
-    assert_true(powers_in_between(1<<12) == 0);
+    assert_true(powers_in_between(PAGE_SIZE, 1<<12) == 0);
 }
 
 void powers_in_between_test2(){
-    assert_true(powers_in_between(1<<13) == 1);
+    assert_true(powers_in_between(PAGE_SIZE, 1<<13) == 1);
 }
 
 void powers_in_between_test3(){
-    assert_true(powers_in_between(1<<16) == 4);
+    assert_true(powers_in_between(PAGE_SIZE, 1<<16) == 4);
 }
 
 void powers_in_between_test4(){
-    assert_true(powers_in_between(1<<11) == -1);
+    assert_true(powers_in_between(PAGE_SIZE, 1<<11) == -1);
 }
 
 void free_lists_creation_test(){
     size_t total_size = (1<<15); //32KB
-    int n_of_lists = powers_in_between(total_size);
+    int n_of_lists = powers_in_between(PAGE_SIZE, total_size);
     int n_of_elems = 1<<n_of_lists;
     int mem[n_of_elems], asst = 0;
     for(int i = 0; i < n_of_elems; i++) mem[i] = i+1;
@@ -74,7 +81,7 @@ void free_lists_creation_test(){
 
 void free_lists_creation_test2(){
     size_t total_size = (1<<16); //64KB
-    int n_of_lists = powers_in_between(total_size);
+    int n_of_lists = powers_in_between(PAGE_SIZE, total_size);
     int n_of_elems = 1<<n_of_lists;
     int mem[n_of_elems], asst = 0;
     for(int i = 0; i < n_of_elems; i++) mem[i] = i+1;
@@ -88,7 +95,7 @@ void free_lists_creation_test2(){
 
 void list_initial_populate_test(){
     size_t total_size = (1<<15); //32KB
-    int n_of_lists = powers_in_between(total_size);
+    int n_of_lists = powers_in_between(PAGE_SIZE, total_size);
     int n_of_elems = 1<<n_of_lists;
     int mem[n_of_elems], asst = 0, aux = 0;
     memset(mem, 0, n_of_elems * sizeof(int));
@@ -103,7 +110,7 @@ void list_initial_populate_test(){
 
 void list_initial_populate_test2(){
     size_t total_size = (1<<16); //64KB
-    int n_of_lists = powers_in_between(total_size);
+    int n_of_lists = powers_in_between(PAGE_SIZE, total_size);
     int n_of_elems = 1<<n_of_lists;
     int mem[n_of_elems], asst = 0, aux = 0;
     memset(mem, 0, n_of_elems * sizeof(int));
@@ -118,7 +125,7 @@ void list_initial_populate_test2(){
 
 void list_initial_populate_test3(){
     size_t total_size = (1<<20); //1MB
-    int n_of_lists = powers_in_between(total_size);
+    int n_of_lists = powers_in_between(PAGE_SIZE, total_size);
     int n_of_elems = 1<<n_of_lists;
     int mem[n_of_elems], asst = 0, aux = 0;
     memset(mem, 0, n_of_elems * sizeof(int));
@@ -143,6 +150,10 @@ void fix_size_test3(){
     assert_true(fix_size(16) == 32);
 }
 
+void fix_size_test4(){
+    assert_true(fix_size(PAGE_SIZE) == PAGE_SIZE<<1);
+}
+
 void power_of_2_check_test(){
     assert_true(IS_POWER_OF_2(2));
 }
@@ -153,4 +164,34 @@ void power_of_2_check_test2(){
 
 void power_of_2_check_test3(){
     assert_false(IS_POWER_OF_2(15));
+}
+
+void alloc_free_list_available_test(){
+    size_t total_size = (1<<15); //32KB
+    void * mem = malloc(total_size), * requested = NULL;
+    int ret = 0;
+    initialize_list(mem, total_size);
+    requested = mem_alloc(PAGE_SIZE<<1);
+    if(requested == NULL){
+        exit(EXIT_FAILURE);
+    }
+    *((int *) requested) = 30;
+    /*printf("base        %p \n", memory_base());
+    printf("returned    %p \n", requested);
+    printf("calculated  %p \n", (int *) memory_base() + (PAGE_SIZE+1) * sizeof(char));
+    printf("%d \n", *((int *) mem + (PAGE_SIZE+1) * sizeof(char)));*/
+    ret =  *((int *) mem + (PAGE_SIZE+1) * sizeof(char)) == 30;
+    free(mem);
+    assert_true(ret);
+}
+
+//helper function
+
+void print_mem(size_t total_size){
+    for(int i = 0; i < powers_in_between(PAGE_SIZE, total_size); i++){
+        for(int j = 0; j < (1<<i);j++){
+            printf("%d ", free_lists[i][j]);
+        }
+        printf("\n");
+    }
 }
