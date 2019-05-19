9fc07b1db8d63f9d3c6a58a7022c4fc95c47fe02
j <jbattilana@itba.edu.ar>
2019-05-19 00:26:14 -0300

    Added queue to scheduler, not working

--- x64barebones/Kernel/scheduler.c
+++ x64barebones/Kernel/scheduler.c
@@ -2,32 +2,41 @@
 #include <mem_manager.h>
 #include <graphics.h>
 #include <interrupts.h>
-
-typedef struct nodeADT *node_t;
-
-typedef struct nodeADT{
-    process_t element;
-    node_t prev;
-    node_t next;
-    int quantum;
-}nodeADT;
+#include <queue.h>
 
 static void set_next_process();
 
+/*
 //LISTA DE NODOS CIRCULAR PARA LOS PROCESOS
 //TODO PROCESO SE AGREGA AL FINAL, SIGUIENDO EL ALGORITMO ROUND ROBIN
 static node_t current_process;
 static node_t first_process;
 static node_t last_process;
 static size_t number_of_processes;
+*/
+
+queue_t scheduler;
 
 void init_scheduler(){
+    scheduler = new_queue();
+    /*
     current_process = NULL;
     last_process = NULL;
     number_of_processes = 0;
+    */
 }
 
 void run_process(process_t process){
+
+    if(get_queue_size(scheduler)==0) {
+        enqueue(scheduler, process);
+        set_state(process, P_RUNNING);
+        _change_process(get_stack_pointer(process));
+    } else {
+        enqueue(scheduler, process);
+    }
+
+    /*
     number_of_processes++;
     if(last_process == NULL){
         last_process = mem_alloc(sizeof(nodeADT));
@@ -50,10 +59,18 @@ void run_process(process_t process){
         last_process->next = aux;
         last_process = aux;
     }
+    */
 }
 
 void kill_current_process(){
 
+    process_t aux = dequeue(scheduler);
+    set_next_process();
+    delete_process(aux);
+    aux = peek(scheduler);
+    _change_process(get_stack_pointer(aux));
+
+    /*
     number_of_processes--;
 
     node_t aux = current_process;
@@ -70,9 +87,25 @@ void kill_current_process(){
     free_mem(aux);
 
     _change_process(get_stack_pointer(current_process->element));
+    */
 }
 
 static void set_next_process(){
+
+    process_t actual = peek(scheduler);
+
+    pstate_t pstate = get_state(actual);
+
+    if(pstate == P_TERMINATE){
+        kill_current_process();
+    }
+    else if(pstate == P_WAITING){
+        set_next_process();
+    }
+    else if(pstate == P_READY){
+        set_state(actual, P_RUNNING);
+    }
+    /*
     if((get_priority(current_process->element) % current_process->quantum) == 0){
         current_process->quantum = 1;
         current_process = current_process->next;
@@ -93,11 +126,22 @@ static void set_next_process(){
     else if(pstate == P_READY){
         set_state(current_process->element, P_RUNNING);
     }
+    */
 }
 
 //Esto se llama desde el iqr handler que seria mas rapido que el timer creo, asi nos acercamos
 // lo mas posible a un quantum como pide la consigna
 uint64_t switch_process(uint64_t stack_pointer){
+    if(get_queue_size(scheduler)>1) {
+        process_t actual = peek(scheduler);
+        set_stack_pointer(actual, stack_pointer);
+        set_next_process();
+        actual = peek(scheduler);
+        return get_stack_pointer(actual);
+    } else {
+        return stack_pointer;
+    }
+    /*
     if(number_of_processes>1){
 
         set_stack_pointer(current_process->element, stack_pointer);
@@ -109,16 +153,21 @@ uint64_t switch_process(uint64_t stack_pointer){
     else {
         return stack_pointer;
     }
+    */
 }
 
 void print_current_processes(){
+    print_queue(scheduler, print_process);
+    /*
     node_t aux = first_process;
     for(int i = 0; i < number_of_processes; i++){
         print_process(aux->element);
         aux = aux->next;
     }
+    */
 }
 
 process_t get_current_process(){
-    return (process_t)current_process->element;
+    //return (process_t)current_process->element;
+    return peek(scheduler);
 }
