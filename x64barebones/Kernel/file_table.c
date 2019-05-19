57d4bdffdf909439642d791c9d4055bf3b0f1398
j <jbattilana@itba.edu.ar>
2019-05-19 19:49:32 -0300

    Created basic init and add of file table

--- x64barebones/Kernel/file_table.c
+++ x64barebones/Kernel/file_table.c
@@ -1,9 +1,29 @@
 #include <file_table.h>
+#include <mem_manager.h>
 
-typedef enum mode_t {READ_ONLY, WRITE_ONLY} mode_t;
+static file_entry_t[MAX_ENTRIES] file_table;
 
 typedef struct file_entry {
     void * entry;
     int offset;
     mode_t mode;
-} file_entry;
+} file_entryADT;
+
+void init_table(){
+    for(int i=0;i < MAX_ENTRIES; i++) {
+        file_table[i]=NULL;
+    }
+}
+
+int add_entry(void * entry, mode_t mode) {
+    int found = -1;
+    for(int i=0; i<MAX_ENTRIES && found!=-1; i++){
+        if(file_table[i]==NULL){
+            file_table[i] = mem_alloc(sizeof(file_entryADT));
+            file_table[i]->offset=0;
+            file_table[i]->mode=mode;
+            found = i;
+        }
+    }
+    return i;
+}
--- x64barebones/Kernel/include/file_table.h
+++ x64barebones/Kernel/include/file_table.h
@@ -1,4 +1,17 @@
 #ifndef _FILE_TABLE_H
 #define _FILE_TABLE_H
 
+#define MAX_ENTRIES 256
+#define PIPE_BUFFER 4096
+
+typedef enum {READ_ONLY, WRITE_ONLY} mode_t;
+
+typedef struct file_entry * file_entry_t;
+
+
+// inits the table with NULL
+void init_table();
+// add an entry to the  and return the number of the entry
+int add_entry(void * entry, mode_t mode);
+
 #endif
