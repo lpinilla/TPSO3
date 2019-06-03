dcbc2bf785ff25b2c72ea92ea58aaa650fa0bdac
JoaquinBattilana <battilanajn@gmail.com>
2019-06-02 23:36:17 -0300

    Now we have a better named pipe example

--- x64barebones/Userland/SampleCodeModule/n_pipes_test.c
+++ x64barebones/Userland/SampleCodeModule/n_pipes_test.c
@@ -1,21 +1,44 @@
 #include <n_pipes_test.h>
 
 static void process_1();
+static void process_2();
 
 void pipe_creation_test(){
-    sys_create_n_pipe("/Hola");
-    int fd = sys_open_fd("/Hola", W_ONLY);
+    print_f("Creando named pipe en el path /Test \n");
+    sys_create_n_pipe("/Test");
+    print_f("Abriendo el fd del proceso main en modo write para el named pipe \n");
+    int fd = sys_open_fd("/Test", W_ONLY);
+    print_f("Escribiendo Hola en el pipe mediante el fd creado \n ");
+    sys_write_fd(fd, "Hola", 4);
+    print_f("Creando procesos 1 y 2 \n");
     sys_create_process(process_1, "Proceso 1", BACKGROUND);
-    sys_write_fd(fd, "Hola", 5);
+    sys_create_process(process_2, "Proceso 2", BACKGROUND);
     sys_close_fd(fd);
-    //sys_delete_file("/Hola");
+    print_f("Proceso principal espera 100 ticks y borra el pipe \n");
+    sys_sleep(100);
+    sys_delete_file("/Hola");
     return;
 }
 
 static void process_1(){
-    char msg[5];
-    int fd = sys_open_fd("/Hola", R_ONLY);
-    sys_read_fd(fd, msg, 5);
-    print_f("%s", msg);
+    char msg[9];
+    msg[8] = 0;
+    print_f("Abriendo el fd en el proceso 1 en modo read para el named pipe \n");
+    int fd = sys_open_fd("/Test", R_ONLY);
+    print_f("Leyendo 8 caracteres, esperamos el Hola y el Chau \n");
+    sys_read_fd(fd, msg, 8);
+    sys_close_fd(fd);
+    print_f("Imprimiendo el string leido del pipe: ");
+    print_f("%s \n", msg);
     return;
+}
+
+static void process_2(){
+    print_f("Abriendo el fd en el proceso 2 en modo write para el named pipe \n");
+    int fd = sys_open_fd("/Test", W_ONLY);
+    print_f("Durmiendo el proceso 2 durante 20 ticks asi el proceso 1 lee y cuando no tiene mas caracteres espera a que el proceso 2 escriba \n");
+    sys_sleep(20);
+    print_f("Escribiendo Chau en el pipe mediante el fd creado \n");
+    sys_write_fd(fd, "Chau", 4);
+    sys_close_fd(fd);
 }
